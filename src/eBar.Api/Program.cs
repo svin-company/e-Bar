8a987ebe3e1fc5b0065eb25bd8b5dc1cd87ff6a7
mycatlikesmilk <my.cat.likes.milk.in.bottle@gmail.com>
2025-09-14 13:19:45 +0300

    init (#2)
    * init
    
    * feat: startup projects
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml

---
+++ src/eBar.Api/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace eBar.Api;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ src/eBar.UnitTests/Main.cs
@@ -0,0 +1,15 @@
+﻿namespace eBar.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void MainTest()
+    {
+        Assert.Pass();
+    }
+}
