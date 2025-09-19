e5354a288eb2cb6badee940357a4f8182e87781a
Olga Malikova <malikovabup@mail.ru>
2025-09-19 20:43:44 +0400

    Сделан интерфейс приложения для официантов

---
+++ src/eBar.WaiterApp/Commands/ConfirmCommand.cs
@@ -0,0 +1,30 @@
+﻿using eBar.WaiterApp.Models;
+using eBar.WaiterApp.Storages;
+
+namespace eBar.WaiterApp.Commands
+{
+    public class ConfirmCommand: BaseCommand
+    {
+        public Order Order { get; set; }
+
+        public bool CanExecute(object parameter) => parameter is Table;
+
+        private readonly Action _onConfirmed;
+
+        public ConfirmCommand(Order order, Action onConfirmed)
+        {
+            Order = order;
+            _onConfirmed = onConfirmed;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter is Table table)
+            {
+                bool result = TableStorage.SaveOrder(table, Order);
+                if (result) _onConfirmed?.Invoke();
+
+            }
+        }
+    }
+}
---
+++ src/eBar.WaiterApp/Storages/TableStorage.cs
@@ -0,0 +1,66 @@
+﻿using eBar.WaiterApp.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace eBar.WaiterApp.Storages
+{
+    class TableStorage
+    {
+        public static readonly ObservableCollection<Table> tables = CreateTables();
+
+        private static ObservableCollection<Table> CreateTables()
+        {
+            ObservableCollection<Table> tables = new ObservableCollection<Table>();
+            for (int i = 0; i < 5; i++)
+            {
+                tables.Add(new Table());
+            }
+            return tables;
+        }
+
+
+
+        public static ObservableCollection<Table> GetAll() => tables;
+
+
+        public static void UpdateOrderStatus(Order order)
+        {
+            var table =  tables.FirstOrDefault(t => t.Orders.Any(o => o.Id == order.Id));
+            var existtingOrder = table.Orders.FirstOrDefault(x => x.Id == order.Id);
+            if (existtingOrder.Status == Order.OrderStatus.Open)
+            {
+                existtingOrder.Status = Order.OrderStatus.Closed;
+            }
+            else
+            {
+                existtingOrder.Status=Order.OrderStatus.Open;
+            }
+        }
+
+        internal static bool SaveOrder(Table table, Order order)
+        {
+            if (order.OrderItems.Count == 0)
+            {
+                MessageBox.Show("Пустой заказ!");
+                return false;
+            }
+
+            var existingTable = tables.Where(x => x.Id == table.Id).FirstOrDefault();
+
+            if (existingTable != null && existingTable.Orders.Count !=0 && HasOpenOrder(existingTable))
+            {
+                MessageBox.Show($"Стол {existingTable.Id} уже имеет незакрытый заказ");
+                return false;
+            }
+
+            existingTable.Orders.Add(order);
+            return true;
+        }
+
+        private static bool HasOpenOrder(Table table)
+        {
+            return  table.Orders.Any(x => x.Status == Order.OrderStatus.Open);
+
+        }
+    }
+}
